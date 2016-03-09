c6087a80e2fd4d0f7fbcce2af584a917f8ceebf9
yetist <xiaotian.wu@i-soft.com.cn>
2016-03-09 21:41:58 +0800

    for deploy

--- iaccount/uwsgi.py
+++ iaccount/uwsgi.py
@@ -12,12 +12,14 @@ import os, sys
 from django.core.wsgi import get_wsgi_application
 
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.insert(0, PROJECT_ROOT)
+PROJECT_NAME = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
 
+sys.path.insert(0, PROJECT_ROOT)
 for i in ("apps", "modules"):
     path_dir = os.path.join(PROJECT_ROOT, i)
     if os.path.isdir(path_dir):
         sys.path.insert(0, path_dir)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, PROJECT_NAME))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
