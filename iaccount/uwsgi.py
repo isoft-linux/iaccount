70d9a6f71bca815f0696fc720fb2bf71289552f7
Wu Xiaotian <xiaotian.wu@i-soft.com.cn>
2016-03-09 17:20:31 +0800

    change for deploy

--- iaccount/uwsgi.py
+++ iaccount/uwsgi.py
@@ -14,6 +14,11 @@ from django.core.wsgi import get_wsgi_application
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(0, PROJECT_ROOT)
 
+for i in ("apps", "modules"):
+    path_dir = os.path.join(PROJECT_ROOT, i)
+    if os.path.isdir(path_dir):
+        sys.path.insert(0, path_dir)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 # This application object is used by any WSGI server configured to use this
--- manage.py
+++ manage.py
@@ -3,7 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
+    PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "iaccount.settings")
+
+    for i in ("apps", "modules"):
+        path_dir = os.path.join(PROJECT_ROOT, i)
+        if os.path.isdir(path_dir):
+            sys.path.insert(0, path_dir)
 
     from django.core.management import execute_from_command_line
 
