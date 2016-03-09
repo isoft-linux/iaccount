7436dc47bd9b93662780b9fac0c880ce82e31e5a
sulit <sulitsrc@gmail.com>
2016-03-09 10:37:27 +0800

    Init iaccount for isoft

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# iaccount
+isoft account system
+====================
+
+fork from django-registration(-redux)
+
+iaccount is a django app
+
+1. pip install django-mama-cas
+
+2. pip install django-registration-redux
+
+3. install this project
---
+++ iaccount/views.py
@@ -0,0 +1,25 @@
+#from registration.users import User
+from registration.forms import User
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+    #userinfo = get_object_or_404(User, pk=request.POST['username'])
+def resetuserinfo(request, user):
+    print(user)
+    print(User.objects.all())
+    userinfo = get_object_or_404(User, username=user)
+    print(userinfo.first_name)
+    print(userinfo.last_name)
+    print(userinfo.email)
+    print(userinfo)
+    userinfo = get_object_or_404(User, username=request.POST['user.username'])
+    print(userinfo)
+    userinfo.first_name = request.POST.get('first_name')
+    print(userinfo.first_name)
+    userinfo.last_name = request.POST.get('last_name')
+    print(userinfo.last_name)
+    userinfo.email = request.post.get('email')
+    print(userinfo.email)
+    userinfo.save()
+    return HttpResponseRedirect(reverse('profile'))
