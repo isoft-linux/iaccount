1491b125c5a70910e9e806a698882829184aebac
sulit <sulitsrc@gmail.com>
2016-03-09 10:56:43 +0800

    update url.py setting.py and profile.html

--- iaccount/urls.py
+++ iaccount/urls.py
@@ -17,13 +17,16 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
+from mama_cas.views import LoginView
+from mama_cas.views import LogoutView
 
 urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^cas/', include('mama_cas.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
-    url(r'^accounts/login', url())
+    url(r'^accounts/login/$', LoginView.as_view(), name='auth_login'),
+    url(r'^accounts/logout/$', LogoutView.as_view(), name='auth_logout'),
     url(r'^accounts/profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
     url(r'^', include('django.contrib.auth.urls')),
 ]
